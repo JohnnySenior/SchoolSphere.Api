e45d7219e03a46722abec3441ce89a302233eeb0
JohnnySenior <jamshidbektursunboev@gmail.com>
2024-08-03 21:01:49 +0900

    Add project files.

---
+++ SchoolSphere.Api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolSphere.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() =>
+            "Hello Mario, Princess is in another castle.";
+    }
+}
