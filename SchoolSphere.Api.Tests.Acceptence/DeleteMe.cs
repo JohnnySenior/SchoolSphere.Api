86415ac5d4c562937a6b87ac086e811a6543101a
JohnnySenior <jamshidbektursunboev@gmail.com>
2024-08-04 18:27:12 +0900

    INFRA: Initialize Acceptence Test

---
+++ SchoolSphere.Api.Tests.Acceptence/DeleteMe.cs
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace SchoolSphere.Api.Tests.Acceptence
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
