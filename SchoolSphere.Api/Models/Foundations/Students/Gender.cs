5875ade26927a05d72376d8532d309a51443343e
JohnnySenior <jamshidbektursunboev@gmail.com>
2024-08-04 19:39:31 +0900

    CODE RUB: Add Header File

--- SchoolSphere.Api/Models/Foundations/Students/Gender.cs
+++ SchoolSphere.Api/Models/Foundations/Students/Gender.cs
@@ -1,4 +1,8 @@
-﻿namespace SchoolSphere.Api.Models.Foundations.Students
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) The Standard Organization: A coalition of the Good-Hearted Engineers
+// ----------------------------------------------------------------------------------
+
+namespace SchoolSphere.Api.Models.Foundations.Students
 {
     public enum Gender
     {
